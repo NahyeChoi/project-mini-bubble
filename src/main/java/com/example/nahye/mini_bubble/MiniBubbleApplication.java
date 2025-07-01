2aef06e14361c61d370321124885070b3879eb50
Nahye Choi <skgp0628@naver.com>
2025-07-01 10:12:59 +0900

    first commit: initial settings

---
+++ src/main/java/com/example/nahye/mini_bubble/MiniBubbleApplication.java
@@ -0,0 +1,13 @@
+package com.example.nahye.mini_bubble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBubbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBubbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nahye/mini_bubble/MiniBubbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nahye.mini_bubble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBubbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
